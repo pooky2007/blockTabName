c0e61f52cdd5c0f94eec931b5785106086c24205
pooky <djamesmassie@gmail.com>
2025-08-08 22:33:17 -0400

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,13 @@
+const unblocked = [
+"idfk"
+];
+
+
+
+
+
+
+
+let currentUrl = window.location.href;
+
+ if (currentUrl != "https://pooky2007.github.io/blocked.html") {window.location.href = "https://pooky2007.github.io/blocked.html"};
---
+++ README.md
@@ -0,0 +1,3 @@
+# SNIFFERS
+
+idk
