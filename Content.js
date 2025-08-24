2aab2637fead6875dfde13e107f0fb9aa66e2be6
pooky <djamesmassie@gmail.com>
2025-08-24 16:35:39 -0400

    Update Content.js

--- Content.js
+++ Content.js
@@ -9,7 +9,7 @@ const unblocked = [
 
 document.title = "google";
 
-
+document.title = "SATAN HERSELF";
 function changeIcon(classname or id) {
     $("i", this).toggleClass("icon-circle-arrow-up icon-circle-arrow-down");
 }
